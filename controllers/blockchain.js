0e1127af6e53b8d982235d1fd8ddd7ddc01fd4e5
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-12-04 18:24:08 +0800

    update

--- controllers/blockchain.js
+++ controllers/blockchain.js
@@ -1,54 +1,5 @@
 const Blockchain = require('../models/blockchain');
 
-const SHA256 = require('crypto-js/sha256');
-const nonce = 0;
-const difficulty = 4;
-
-exports.mineBlock = () => {
-    while (req.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-        nonce++;
-        hash = req.hash;
-    }
-}
-
-// /**
-//  * create genesis block for first instance of patient created
-//  * * @param patient Address
-//  */
-// exports.createGenesisBlock = async(req, res, next) => {
-
-//         let genesisBlock = {
-//             patient: 'myaddress', // req.patientAddress
-//             message: 'Genesis Block'
-//         };
-
-//         const newBlock = {
-//             timestamp: req.timestamp,
-//             previousHash: req.lastBlock.hash,
-//             transactions: genesisBlock,
-//             hash: req.calculateHash
-//         };
-
-//         let block = await newBlock.save();
-//         if (!block) {
-//             throw new Error('Something went wrong.Cannot save new block!');
-//         }
-
-//         next();
-//     }
-/**
- * get the last block of the chain
- */
-// exports.getLastBlock = async(req, res, next) => {
-
-//     let lastBlock = Blockchain.findOne({}, null, { sort: { _id: -1 }, limit: 1 });
-//     if (!lastBlock) {
-//         throw new Error('Something went wrong.Cannot get th last block!');
-//     }
-
-//     return lastBlock;
-// }
-
 exports.getChain = async(req, res, next) => {
     try {
         const pageSize = +req.query.pagesize;
