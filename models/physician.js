960f442b0862fd842cc4eea6acde958c8f339a5d
randy rebucas <rebucasrandy1986@gmail.com>
2020-03-03 06:48:00 +0800

    update

--- models/physician.js
+++ models/physician.js
@@ -1,15 +1,14 @@
 const mongoose = require('../db/index');
 
 const practiceSchema = mongoose.Schema({
-    title: { type: String, default: null }
+    title: { type: String, default: null },
+    year: { type: Number, default: null }
 });
 
 const schema = mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users' },
-    practice: String,
-    yearsExperience: Number,
-    description: String,
     practices: [practiceSchema],
+    description: String,
     prc: { type: String, default: null },
     ptr: { type: String, default: null },
     s2: { type: String, default: null },
