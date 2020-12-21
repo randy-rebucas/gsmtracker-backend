a69a7af36f45bb74cb3c70d62ff44f4fc220988d
Randy rebucas <rebucasrandy1986@gmail.com>
2020-12-21 12:35:43 +0800

    heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'gsm Tracker' });
     // res.send('Hello ' + JSON.stringify(req.session));
 });
 
